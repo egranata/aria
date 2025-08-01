6afa7dff4b9b0408376b2707f989d592bf3f5a85
Enrico Granata <egranata@aria>
2025-07-31 21:34:59 -0600

    Add else {} block support to for and while statements in the grammar

--- parser-lib/src/ast/nodes/while_statement.rs
+++ parser-lib/src/ast/nodes/while_statement.rs
@@ -1,15 +1,34 @@
 // SPDX-License-Identifier: Apache-2.0
 use crate::{
     ast::{
-        CodeBlock, Expression, WhileStatement,
+        CodeBlock, ElsePiece, Expression, WhileStatement,
         derive::Derive,
         prettyprint::{PrettyPrintable, printout_accumulator::PrintoutAccumulator},
     },
-    gen_from_components,
+    grammar::Rule,
 };
 
 impl Derive for WhileStatement {
-    gen_from_components!(while_stmt; cond: Expression, then: CodeBlock);
+    fn from_parse_tree(
+        p: pest::iterators::Pair<'_, crate::grammar::Rule>,
+        source: &crate::ast::SourceBuffer,
+    ) -> Self {
+        assert!(p.as_rule() == Rule::while_stmt);
+        let loc = From::from(&p.as_span());
+        let mut inner = p.into_inner();
+        let cond = Expression::from_parse_tree(inner.next().expect("need condition"), source);
+        let then = CodeBlock::from_parse_tree(inner.next().expect("need then block"), source);
+        let els = inner
+            .next()
+            .map(|else_p| ElsePiece::from_parse_tree(else_p, source));
+
+        Self {
+            loc: source.pointer(loc),
+            cond,
+            then,
+            els,
+        }
+    }
 }
 
 impl PrettyPrintable for WhileStatement {
