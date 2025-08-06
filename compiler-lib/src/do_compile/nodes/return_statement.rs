d4b2409c156e535b3fb5db85ed2eb428109eef43
Enrico Granata <egranata@aria>
2025-08-05 21:40:29 -0600

    Allow return with no expression
    By default, return; will bring back Unit::unit, same as a fallthrough function

--- compiler-lib/src/do_compile/nodes/return_statement.rs
+++ compiler-lib/src/do_compile/nodes/return_statement.rs
@@ -1,12 +1,32 @@
 // SPDX-License-Identifier: Apache-2.0
+use haxby_opcodes::builtin_type_ids::BUILTIN_TYPE_UNIT;
+
 use crate::{
+    constant_value::ConstantValue,
     do_compile::{CompilationResult, CompileNode, CompileParams},
     func_builder::BasicBlockOpcode,
 };
 
 impl<'a> CompileNode<'a> for aria_parser::ast::ReturnStatement {
     fn do_compile(&self, params: &'a mut CompileParams) -> CompilationResult {
-        self.val.do_compile(params)?;
+        if let Some(val) = &self.val {
+            val.do_compile(params)?;
+        } else {
+            let unit_name =
+                self.insert_const_or_fail(params, ConstantValue::String("unit".into()), &self.loc)?;
+
+            params
+                .writer
+                .get_current_block()
+                .write_opcode_and_source_info(
+                    BasicBlockOpcode::PushBuiltinTy(BUILTIN_TYPE_UNIT),
+                    self.loc.clone(),
+                )
+                .write_opcode_and_source_info(
+                    BasicBlockOpcode::NewEnumVal(unit_name),
+                    self.loc.clone(),
+                );
+        }
         params
             .writer
             .get_current_block()
--- parser-lib/src/ast/nodes/return_statement.rs
+++ parser-lib/src/ast/nodes/return_statement.rs
@@ -1,19 +1,32 @@
 // SPDX-License-Identifier: Apache-2.0
 use crate::{
     ast::{
-        Expression, ReturnStatement,
+        Expression, ReturnStatement, SourceBuffer,
         derive::Derive,
         prettyprint::{PrettyPrintable, printout_accumulator::PrintoutAccumulator},
     },
-    gen_from_components,
+    grammar::Rule,
 };
 
 impl Derive for ReturnStatement {
-    gen_from_components!(return_stmt; val: Expression);
+    fn from_parse_tree(p: pest::iterators::Pair<'_, Rule>, source: &SourceBuffer) -> Self {
+        assert!(p.as_rule() == Rule::return_stmt);
+        let loc = From::from(&p.as_span());
+        let mut inner = p.into_inner();
+        let val = inner.next().map(|p| Expression::from_parse_tree(p, source));
+        Self {
+            loc: source.pointer(loc),
+            val,
+        }
+    }
 }
 
 impl PrettyPrintable for ReturnStatement {
     fn prettyprint(&self, buffer: PrintoutAccumulator) -> PrintoutAccumulator {
-        buffer << "return " << &self.val << ";"
+        let mut buffer = buffer << "return";
+        if let Some(val) = &self.val {
+            buffer = buffer << " " << val;
+        }
+        buffer << ";"
     }
 }
