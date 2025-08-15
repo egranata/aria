d1e9bc8b202e5159f39b94938cd25b4278606615
Enrico Granata <egranata@aria>
2025-08-14 18:08:55 -0600

    Improve the way error reporting is generated and printed
    This is no functional change but it paves the way for the REPL to go and do
    something interesting with its errors, which can then be used by test cases
    to do something interesting by feeding input to the REPL and watching its
    output go by

--- aria-bin/src/error_reporting.rs
+++ aria-bin/src/error_reporting.rs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-use std::collections::HashMap;
+use std::{collections::HashMap, vec};
 
 use aria_compiler::do_compile::CompilationError;
 use aria_parser::ast::{ParserError, SourcePointer};
@@ -15,44 +15,53 @@ pub struct StringCache {
     buffers: HashMap<String, Source>,
 }
 
-impl ariadne::Cache<&String> for StringCache {
+impl ariadne::Cache<String> for StringCache {
     type Storage = String;
 
     fn fetch(
         &mut self,
-        path: &&String,
-    ) -> Result<
-        &Source<<Self as ariadne::Cache<&std::string::String>>::Storage>,
-        impl std::fmt::Debug,
-    > {
-        Ok::<&Source, Source>(&self.buffers[*path])
+        path: &String,
+    ) -> Result<&Source<<Self as ariadne::Cache<String>>::Storage>, impl std::fmt::Debug> {
+        Ok::<&Source, Source>(&self.buffers[path])
     }
 
     #[allow(refining_impl_trait)]
-    fn display<'a>(&self, path: &&'a String) -> Option<impl std::fmt::Display + 'a> {
-        Some(Box::new((**path).clone()))
+    fn display<'a>(&self, path: &'a String) -> Option<impl std::fmt::Display + 'a> {
+        Some(Box::new((*path).clone()))
     }
 }
 
-fn report_from_msg_and_location(msg: &str, locations: &[&SourcePointer]) {
+pub(crate) type PrintableReport<'a> = (
+    ariadne::Report<'a, (std::string::String, std::ops::Range<usize>)>,
+    StringCache,
+);
+
+fn build_report_from_msg_and_location<'a>(
+    msg: &str,
+    locations: Vec<SourcePointer>,
+) -> PrintableReport<'a> {
     let config = ariadne::Config::default().with_index_type(ariadne::IndexType::Byte);
     let magenta = Color::Magenta;
     let primary_span = &locations[0];
     let mut report = Report::build(
         ReportKind::Error,
         (
-            &primary_span.buffer.name,
+            primary_span.buffer.name.clone(),
             primary_span.location.start..primary_span.location.stop,
         ),
     )
     .with_message(msg)
     .with_config(config);
     let mut cache = StringCache::default();
-    for loc in locations {
+    for loc in &locations {
+        let loc = loc.clone();
         report = report.with_label(
-            Label::new((&loc.buffer.name, loc.location.start..loc.location.stop))
-                .with_message("here")
-                .with_color(magenta),
+            Label::new((
+                loc.buffer.name.clone(),
+                loc.location.start..loc.location.stop,
+            ))
+            .with_message("here")
+            .with_color(magenta),
         );
         if !cache.buffers.contains_key(&loc.buffer.name) {
             cache.buffers.insert(
@@ -61,38 +70,63 @@ fn report_from_msg_and_location(msg: &str, locations: &[&SourcePointer]) {
             );
         }
     }
-    report.finish().eprint(cache).unwrap();
+    (report.finish(), cache)
 }
 
-pub(crate) fn report_from_vm_error(err: &VmError) {
+pub(crate) fn print_report_from_vm_exception(vm: &mut VirtualMachine, exc: &VmException) {
+    let (report, cache) = build_report_from_vm_exception(vm, exc);
+    report.eprint(cache).unwrap();
+}
+
+pub(crate) fn print_report_from_compiler_error(err: &CompilationError) {
+    let (report, cache) = build_report_from_compiler_error(err);
+    report.eprint(cache).unwrap();
+}
+
+pub(crate) fn print_report_from_parser_error(err: &ParserError) {
+    let (report, cache) = build_report_from_parser_error(err);
+    report.eprint(cache).unwrap();
+}
+
+pub(crate) fn print_report_from_vm_error(err: &VmError) {
+    let (report, cache) = build_report_from_vm_error(err);
+    report.eprint(cache).unwrap();
+}
+
+pub(crate) fn build_report_from_vm_error<'a>(err: &VmError) -> PrintableReport<'a> {
     let msg = err.reason.to_string();
     if err.backtrace.is_empty() {
         if let Some(loc) = &err.loc {
-            report_from_msg_and_location(&msg, &[loc]);
+            build_report_from_msg_and_location(&msg, vec![loc.clone()])
         } else {
-            eprintln!("vm execution error: {msg}");
+            build_report_from_msg_and_location(&msg, vec![])
         }
     } else {
-        let backtraces: Vec<_> = err.backtrace.entries_iter().collect();
-        report_from_msg_and_location(&msg, &backtraces);
+        let backtraces: Vec<_> = err.backtrace.entries_iter().cloned().collect();
+        build_report_from_msg_and_location(&msg, backtraces)
     }
 }
 
-pub(crate) fn report_from_vm_exception(vm: &mut VirtualMachine, exc: &VmException) {
+pub(crate) fn build_report_from_vm_exception<'a>(
+    vm: &mut VirtualMachine,
+    exc: &'a VmException,
+) -> PrintableReport<'a> {
     let mut cur_frame = Frame::default();
     let msg = exc.value.prettyprint(&mut cur_frame, vm);
-    let backtraces: Vec<_> = exc.backtrace.entries_iter().collect();
-    report_from_msg_and_location(&msg, backtraces.as_slice());
+    let backtraces: Vec<_> = exc.backtrace.entries_iter().cloned().collect();
+    build_report_from_msg_and_location(&msg, backtraces)
 }
 
-pub(crate) fn report_from_compiler_error(err: &CompilationError) {
+pub(crate) fn build_report_from_compiler_error<'a>(
+    err: &'a CompilationError,
+) -> PrintableReport<'a> {
     let msg = err.reason.to_string();
     let loc = &err.loc;
-    report_from_msg_and_location(&msg, &[loc]);
+    build_report_from_msg_and_location(&msg, vec![loc.clone()])
 }
 
-pub(crate) fn report_from_parser_error(err: &ParserError) {
+pub(crate) fn build_report_from_parser_error<'a>(err: &'a ParserError) -> PrintableReport<'a> {
     let msg = &err.msg;
     let loc = &err.loc;
-    report_from_msg_and_location(msg, &[loc]);
+    build_report_from_msg_and_location(msg, vec![loc.clone()])
 }
