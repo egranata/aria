2977b92858067f04d98cb11f476519a121afb221
Enrico Granata <egranata@users.noreply.github.com>
2025-08-26 14:11:01 -0600

    Allow short-hand initializer syntax (#142)
    Fixes #89
    
    ---------
    
    Co-authored-by: Enrico Granata <egranata@aria>

--- parser-lib/src/ast/nodes/postfix_term_field_set.rs
+++ parser-lib/src/ast/nodes/postfix_term_field_set.rs
@@ -1,19 +1,35 @@
 // SPDX-License-Identifier: Apache-2.0
 use crate::{
     ast::{
-        Expression, Identifier, PostfixTermFieldSet,
+        Expression, Identifier, PostfixTermFieldSet, SourceBuffer,
         derive::Derive,
         prettyprint::{PrettyPrintable, printout_accumulator::PrintoutAccumulator},
     },
-    gen_from_components,
+    grammar::Rule,
 };
 
 impl Derive for PostfixTermFieldSet {
-    gen_from_components!(postfix_field_set; id: Identifier, val: Expression);
+    fn from_parse_tree(p: pest::iterators::Pair<'_, Rule>, source: &SourceBuffer) -> Self {
+        assert!(p.as_rule() == Rule::postfix_field_set);
+        let loc = From::from(&p.as_span());
+        let mut inner = p.into_inner();
+        let id = Identifier::from_parse_tree(inner.next().expect("postfix need id"), source);
+        let val = inner.next().map(|p| Expression::from_parse_tree(p, source));
+        Self {
+            loc: source.pointer(loc),
+            id,
+            val,
+        }
+    }
 }
 
 impl PrettyPrintable for PostfixTermFieldSet {
     fn prettyprint(&self, buffer: PrintoutAccumulator) -> PrintoutAccumulator {
-        buffer << "." << &self.id << " = " << &self.val
+        let buffer = buffer << "." << &self.id;
+        if let Some(val) = &self.val {
+            buffer << " = " << val
+        } else {
+            buffer
+        }
     }
 }
