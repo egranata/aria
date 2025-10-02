64a4b3397fe8e8e6d52a0cedb4e1602cf27a7715
Enrico Granata <egranata@aria>
2025-10-01 22:15:59 -0600

    Enable index operators [] and []= to take no arguments

--- parser-lib/src/ast/nodes/postfix_term_index_write.rs
+++ parser-lib/src/ast/nodes/postfix_term_index_write.rs
@@ -1,15 +1,37 @@
 // SPDX-License-Identifier: Apache-2.0
-use crate::{
-    ast::{
-        Expression, ExpressionList, PostfixTermIndexWrite,
-        derive::Derive,
-        prettyprint::{PrettyPrintable, printout_accumulator::PrintoutAccumulator},
-    },
-    gen_from_components,
+use crate::ast::{
+    Expression, ExpressionList, PostfixTermIndexWrite,
+    derive::Derive,
+    prettyprint::{PrettyPrintable, printout_accumulator::PrintoutAccumulator},
 };
 
 impl Derive for PostfixTermIndexWrite {
-    gen_from_components!(postfix_term_index_write; idx: ExpressionList, val: Expression);
+    fn from_parse_tree(
+        p: pest::iterators::Pair<'_, crate::grammar::Rule>,
+        source: &crate::ast::SourceBuffer,
+    ) -> Self {
+        assert!(p.as_rule() == crate::grammar::Rule::postfix_term_index_write);
+        let loc = From::from(&p.as_span());
+        let mut inner = p.into_inner();
+        let idx = if inner.peek().expect("need index").as_rule() == crate::grammar::Rule::expr_list
+        {
+            <ExpressionList>::from_parse_tree(
+                inner.next().expect(concat!("need ", stringify!(idx))),
+                source,
+            )
+        } else {
+            ExpressionList::empty(source.pointer(loc))
+        };
+        let val = <Expression>::from_parse_tree(
+            inner.next().expect(concat!("need ", stringify!(val))),
+            source,
+        );
+        Self {
+            loc: source.pointer(loc),
+            idx,
+            val,
+        }
+    }
 }
 
 impl PrettyPrintable for PostfixTermIndexWrite {
