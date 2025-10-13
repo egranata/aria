bc3561854580ce1886612eb1c2a29f80b9cc002e
Enrico Granata <egranata@aria>
2025-10-13 16:27:51 -0600

    Include aria_timezone in the release packages

--- package.sh
+++ package.sh
@@ -2,6 +2,7 @@ set -euo pipefail
 
 PROFILE=${PROFILE:-dist}
 
+# The authoritative copy of these variables is in .github/workflows/release.yml
 BIN_TARGETS="${BIN_TARGETS:-aria}"
 DYLIB_CRATES="${DYLIB_CRATES:-aria_file aria_http aria_path aria_platform aria_regex aria_timezone}"
 EXTRA_FILES="${EXTRA_FILES:-}"
