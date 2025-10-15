e6e4dffbbcb590a86be8bb73757ca0f9904c81c4
Borja Castellano <borjacastellano1@gmail.com>
2025-10-14 21:05:18 -0700

    Path::get_filename  returns Maybe

--- native-libs/path/src/lib.rs
+++ native-libs/path/src/lib.rs
@@ -761,7 +761,12 @@ impl BuiltinFunctionImpl for Filename {
         match rfo.file_name() {
             Some(name) => {
                 let name = some_or_err!(name.to_str(), VmErrorReason::UnexpectedVmState.into());
-                frame.stack.push(RuntimeValue::String(name.into()));
+                let val = ok_or_err!(
+                    vm.builtins
+                        .create_maybe_some(RuntimeValue::String(name.into())),
+                    VmErrorReason::UnexpectedVmState.into()
+                );
+                frame.stack.push(val);
             }
             None => {
                 let val = ok_or_err!(
