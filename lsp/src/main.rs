2a121ad57248e837e2ff82a00987f761c8feadff
Ajan <ajprabakara@proton.me>
2025-10-23 17:27:06 -0400

    set up server

---
+++ lsp/src/main.rs
@@ -0,0 +1,36 @@
+use tower_lsp::jsonrpc::Result;
+use tower_lsp::lsp_types::*;
+use tower_lsp::{Client, LanguageServer, LspService, Server};
+
+struct Backend {
+    client: Client
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult {
+            server_info: None,
+            capabilities: ServerCapabilities{
+                ..ServerCapabilities::default()
+            }
+        })
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+
+    let (service, socket) = LspService::build(|client| Backend {
+        client
+    })
+    .finish();
+
+    Server::new(stdin, stdout, socket).serve(service).await;
+}
