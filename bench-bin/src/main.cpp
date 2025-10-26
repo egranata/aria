8077d26448be54ad99c519ac72347725b0f5ed14
Borja Castellano <borjacastellano1@gmail.com>
2025-10-26 22:45:26 +0000

    nanobench wrapper writes the results of the benchmark

--- bench-bin/src/main.cpp
+++ bench-bin/src/main.cpp
@@ -5,6 +5,7 @@
 #include <ostream>
 #include <sstream>
 #include <string>
+#include <unistd.h>
 #include <vector>
 #define ANKERL_NANOBENCH_IMPLEMENT
 #include "/home/borja/projects/aria/bench-bin/deps/nanobench/src/include/nanobench.h"
@@ -24,7 +25,21 @@ std::vector<std::filesystem::path> split(const std::string &s, char delim) {
   return elems;
 }
 
-void exec_bench_on_file(Bench bench, std::filesystem::path src);
+void exec_bench_on_file(Bench &bench, std::filesystem::path src) {
+  std::string command_string = "./target/release/aria " + src.string();
+  const char *command = command_string.c_str();
+
+  bench.run(src.c_str(), [&] { int _ = std::system(command); });
+}
+
+void write_output(std::filesystem::path folder, std::string const &typeName,
+                  char const *mustacheTemplate, Bench const &bench) {
+  std::filesystem::create_directories(folder);
+
+  std::ofstream resultsOut(folder / ("mustache.render." + typeName));
+
+  ankerl::nanobench::render(mustacheTemplate, bench, resultsOut);
+}
 
 int main(int argc, char *argv[]) {
 
@@ -43,6 +58,8 @@ int main(int argc, char *argv[]) {
 
   Bench bench = Bench();
 
+  bench.title("Benchmarking Aria execution time");
+
   for (const std::filesystem::path dir : bench_dirs) {
     for (const auto &entry : std::filesystem::directory_iterator(dir)) {
       if (entry.is_regular_file() &&
@@ -52,11 +69,30 @@ int main(int argc, char *argv[]) {
       }
     }
   }
-}
 
-void exec_bench_on_file(Bench bench, std::filesystem::path src) {
-  std::string command_string = "./target/release/aria " + src.string();
-  const char *command = command_string.c_str();
+  std::filesystem::path results_folder = "target/nanobench/results";
 
-  bench.run(src.c_str(), [&] { int _ = std::system(command); });
+  write_output(results_folder, "html", templates::htmlBoxplot(), bench);
+  write_output(results_folder, "csv", templates::csv(), bench);
+  write_output(results_folder, "json", templates::json(), bench);
+
+  // Print comparison here with the last baseline
+
+  // Ask if the user want to udpate the baseline results with the current
+  // results
+
+  printf("\nDo you want to save the new results as the next baseline?? [y/N] ");
+
+  std::string input;
+  std::getline(std::cin, input);
+
+  if (!input.empty() && (input[0] == 'y' || input[0] == 'Y')) {
+    std::filesystem::path baseline_folder = "target/nanobench/baseline";
+
+    write_output(baseline_folder, "csv", templates::csv(), bench);
+    write_output(baseline_folder, "json", templates::json(), bench);
+    write_output(baseline_folder, "html", templates::htmlBoxplot(), bench);
+  }
+
+  return 0;
 }
