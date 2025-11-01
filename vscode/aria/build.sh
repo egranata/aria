da65e2942df484300b07327b09bc5e4fd228d40f
Ajan <ajprabakara@proton.me>
2025-11-01 19:48:50 -0400

    allow customization of editor installation + vscodeignore

--- vscode/aria/build.sh
+++ vscode/aria/build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+[ -f ./.env ] && . ./.env
+
+EDITOR_CLI="${EDITOR_CLI:-${EDITOR_CMD:-${EDITOR:-${VSCODE_CLI:-code}}}}"
+
 cargo build --manifest-path ../../lsp/Cargo.toml && \
 vsce package --allow-missing-repository && \
-code --install-extension ./aria-0.0.1.vsix
+"$EDITOR_CLI" --install-extension ./aria-0.0.1.vsix
 
 # sadly, the vscode window will still need to be reloaded after running this
