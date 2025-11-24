0b1a21e8169682bdd9145a33913d691d7a3018f3
Enrico Granata <egranata@aria>
2025-11-24 12:47:23 -0700

    Accept inner.len() clippy
    For some reason #383 complains about this

--- parser-lib/src/ast/nodes/mul_operation.rs
+++ parser-lib/src/ast/nodes/mul_operation.rs
@@ -9,6 +9,7 @@ use crate::{
 };
 
 impl Derive for MulOperation {
+    #[allow(clippy::len_zero)]
     fn from_parse_tree(p: pest::iterators::Pair<'_, Rule>, source: &SourceBuffer) -> Self {
         assert!(p.as_rule() == Rule::mul);
         let loc = From::from(&p.as_span());
